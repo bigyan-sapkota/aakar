4f7690595da77cb85e3361225e8818d8252d4611
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-10-03 18:45:48 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// import React from "react";
+
+// export default function App() {
+//   return (
+//     <div className="group flex w-screen flex-col items-center justify-center gap-40 border-4 border-blue-700 lg:flex-row">
+//       <div className="text-xl font-bold">Hello World</div>
+//       <div className="size-40 bg-red-400 hover:cursor-pointer group-hover:bg-orange-500 lg:bg-blue-700"></div>
+//       <button className="custom-transition border border-white bg-blue-700 px-8 py-2 text-lg font-semibold text-white hover:border hover:border-blue-700 hover:bg-white hover:text-blue-700">
+//         Hello
+//       </button>
+//     </div>
+//   );
+// }
+
+// import React from "react";
+
+// export default function App() {
+//   return (
+//     <div className="flex h-screen items-center justify-center px-10 py-20">
+//       <div className="flex h-full w-full gap-10 border-4 border-red-700">
+//         <div className="h-full border-4 border-blue-700 bg-blue-700">
+//           <img
+//             src="./home/sample.jpg"
+//             alt=""
+//             className="h-full w-full object-cover"
+//           />
+//         </div>
+//         <div className="h-full w-2/3 border-4 border-green-600 bg-green-600">
+//           <img
+//             src="./home/sample.jpg"
+//             alt=""
+//             className="h-full w-full object-cover"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
